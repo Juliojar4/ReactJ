fd4108fbb089d7b6ab0154b6f6c704c93cacb8be
Dionísio <dionisio@Dionisio.local>
2022-02-21 18:01:00 -0300

    descrição do commit

--- myHooks/my_hooks/src/views/examples/UseState.jsx
+++ myHooks/my_hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/PageTitle'
+import SectionTitle from '../../components/SectionTitle'
 
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    /* 
+    Mesma coisa que:
+    const num = array[0]
+    const setNum = array[1]
+    */
+
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercício #01" />
+                <div className="center">
+                    <span className="text">{count}</span>
+                <div>
+                    <button className="btm"
+                    onClick={() => setCount(count + 1)}>+1</button>
+                    <button className="btm"
+                        onClick={() => setCount(count - 1)}>-1</button>
+                    <button className="btm"
+                        onClick={() => setCount(current => current + 100)}>100</button>
+                </div>
+                </div> 
+            <SectionTitle title="Exercício #02" />
+            <input type="text" className="input"
+                value={name} onChange={(e) => setName(e.target.value)} />
+            {name}
         </div>
     )
 }
